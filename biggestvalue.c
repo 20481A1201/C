5f12b3f9f648a8d20962ba598c632b79d5b01c08
20481A1201 <99742941+20481A1201@users.noreply.github.com>
2023-07-10 21:25:03 +0530

    Create biggestvalue.c

---
+++ biggestvalue.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void printbignum1(int a,int b);
+void printbignum2(int a,int b);
+int main() {
+    int a,b;
+    printf("Enter a value:");
+    scanf("%d",&a);
+    printf("Enter b value:");
+    scanf("%d",&b);
+    printbignum1(a,b);
+    printbignum2(a,b);
+    
+
+    return 0;
+}
+void printbignum1(int a,int b){
+    printf("\n Using if elseif block!");
+    if(a>b){
+        printf("\na is big where a=%d,b=%d",a,b);
+    }
+    else{
+        printf("\nb is big where b=%d,a=%d",b,a);
+    }
+}
+void printbignum2(int a,int b){
+    printf("\nUsing ternary operator! ");
+    printf("\na value is %d",a);
+    printf("\nb value is %d",b);
+    printf("\nBiggest value is %d",(a>b)?a:b);
+}
