9c466822faac5d36fbc8deaee2b9ec56cf4387b6
20481A1201 <99742941+20481A1201@users.noreply.github.com>
2023-07-10 21:30:49 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a[3]={0x01,0xF4,0x10001};
+    int cnt=0;
+    int i;
+    for(i=0;i<3;i++){
+        while(a[i]>0){
+            a[i]=a[i]&(a[i]-1);
+            cnt++;
+        }    
+    }
+    printf("\ncnt is %d",cnt);
+    return 0;
+}
